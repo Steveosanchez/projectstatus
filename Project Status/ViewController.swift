e5b62a757a88f5dad8b52b4148ce9e9276f5aba8
Steven Sanchez <steven.sanchez@sephora.com>
2018-04-03 21:40:27 -0700

    Initial Commit

---
+++ Project Status/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project Status
+//
+//  Created by Sanchez, Steven on 4/3/18.
+//  Copyright © 2018 Sanchez, Steven. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
